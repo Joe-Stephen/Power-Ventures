d2f36e140d3c38503dd5869cec4991f24ac94e87
Joe Stephen <joestephenk10@gmail.com>
2025-10-30 00:08:00 +0530

    Add FeaturedWorks and WhyUs components to App, enhancing project showcase and reasons for selection.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
 import Products from './components/Products';
+import FeaturedWorks from './components/FeaturedWorks';
+import WhyUs from './components/WhyUs';
 import Testimonials from './components/Testimonials';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -16,6 +18,8 @@ function App() {
       <Hero />
       <Brands />
       <Products />
+      <FeaturedWorks />
+      <WhyUs />
       <Testimonials />
       <About />
       <Contact />
